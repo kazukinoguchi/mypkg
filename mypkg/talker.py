d16aeb0a641be5b77acc3bd05cb3ff247178096d
Kazuki Noguchi <winniegorst@gmail.com>
2023-12-01 09:42:50 +0900

    Updated

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -3,19 +3,18 @@ from rclpy.node import Node      #ノードを実装するためのNodeクラス
 from std_msgs.msg import Int16   #通信の型（16ビットの符号付き整数）
 
 class Talker():          #ヘッダの下にTalkerというクラスを作成
-    def __init__(self):  # オブジェクトを作ると呼ばれる関数
+    def __init__(self, node):  # オブジェクトを作ると呼ばれる関数
         self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
+        node.create_timer(0.5, self.cb)
+
+    def cb(self):              #関数内のnやpubをtalkerのものに変更
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
 
 rclpy.init()
 node = Node("talker")
-talker = Talker()      #オブジェクトを作成（__init__が実行される。）
-
-def cb():              #関数内のnやpubをtalkerのものに変更
-    msg = Int16()
-    msg.data = talker.n
-    talker.pub.publish(msg)
-    talker.n += 1
-
-node.create_timer(0.5, cb)  #タイマー設定
+talker = Talker(node)      #オブジェクトを作成（__init__が実行される。）
 rclpy.spin(node)            #実行（無限ループ）
