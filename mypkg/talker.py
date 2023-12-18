795a07cfbc830a2e9e83ec62bd4475808aac5bc0
Kazuki Noguchi <winniegorst@gmail.com>
2023-12-18 21:37:52 +0900

    edit talker.py to send comments

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,16 +1,20 @@
+# SPDX-FileCopyRightText: 2023 Kazuki Noguchi
+# SPDX-License-Identifire: BSD-3-Clause
+
 import rclpy                     #ROS 2のクライアントのためのライブラリ
 from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
-from std_msgs.msg import Int16   #通信の型（16ビットの符号付き整数）
+from std_msgs.msg import String   #通信の型（16ビットの符号付き整数）
 
 class Talker():          #ヘッダの下にTalkerというクラスを作成
     def __init__(self, node):  # オブジェクトを作ると呼ばれる関数
-        self.pub = node.create_publisher(Int16, "countup", 10)
-        self.n = 0
-        node.create_timer(0.5, self.cb)
+        self.pub = node.create_publisher(String, "Comments", 10)
+        #self.n = 0
+        node.create_timer(0.01, self.cb)
 
     def cb(self):              #関数内のnやpubをtalkerのものに変更
-        msg = Int16()
-        msg.data = self.n
+        msg = String()
+        #msg.data = self.n
+        msg.data = input('Comments here: ')
         self.pub.publish(msg)
         self.n += 1
 
